2999fa0c02b1c085c91c2363e5e1cefaf01c29db
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-15 17:50:24 +0530

    showing recipe list using *ngFor directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,6 +9,7 @@ import {Recipe} from "../Recipe.model";
 export class RecipeListComponent implements OnInit {
 
   recipes : Recipe[] = [
+    new Recipe('Test Recipe','for test purpose','https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
     new Recipe('Test Recipe','for test purpose','https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
   ];
 
