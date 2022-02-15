92ccd1bef7cbba9a5c0d1cc779af3376a3800b5e
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-02-15 15:53:27 +0530

    recipe list view template

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../Recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes : Recipe[] = [
+    new Recipe('Test Recipe','for test purpose','https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+  ];
 
   constructor() { }
 
