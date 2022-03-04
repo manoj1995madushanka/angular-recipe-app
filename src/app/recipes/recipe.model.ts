8a26dd030885975c41180d9e3bd452dcec0a521d
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-03-04 09:58:06 +0530

    add ingredient list to recipes and display

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,15 +1,18 @@
-
 /**
  * model that contains recipe data
  * */
+import {Ingredient} from "../shared/ingredient.model";
+
 export class Recipe{
   public name:string;
   public description:string;
   public imagePath:string;
+  public ingredients: Ingredient[];
 
-  constructor(name: string, description: string, imagePath: string) {
+  constructor(name: string, description: string, imagePath: string, ingredients: Ingredient[]) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
+    this.ingredients = ingredients;
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,21 +1,30 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from "./recipe.model";
+import {Ingredient} from "../shared/ingredient.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  private recipes : Recipe[] = [
-    new Recipe('Test Recipe 01','for test purpose 01','https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg'),
-    new Recipe('Test Recipe 02','for test purpose 02','https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg')
+  private recipes: Recipe[] = [
+    new Recipe('Test Recipe 01', 'for test purpose 01',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg', [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 10)
+      ]),
+    new Recipe('Test Recipe 02', 'for test purpose 02',
+      'https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg', [
+        new Ingredient('Cheese', 2)
+      ])
   ];
 
   recipeSelected = new EventEmitter<Recipe>();
 
-  constructor() { }
+  constructor() {
+  }
 
-  getRecipes(){
+  getRecipes() {
     return this.recipes.slice(); // return duplicate list of recipes
   }
 }
