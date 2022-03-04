6840a1d01877e5170c88073bc9b13093ecf157e2
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-03-05 03:25:02 +0530

    change forms to template driven

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {Ingredient} from "../../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-shopping-edit',
@@ -9,8 +10,11 @@ import {ShoppingListService} from "../shopping-list.service";
 })
 export class ShoppingEditComponent implements OnInit {
 
-  @ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
-  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;
+  // commented view child because we move to template driven form approach
+  /*@ViewChild('nameInput', {static: false}) nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: false}) amountInputRef: ElementRef;*/
+
+
   //@Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor(private shoppingLisService: ShoppingListService) {
@@ -19,10 +23,14 @@ export class ShoppingEditComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddItem() {
-    const name = this.nameInputRef.nativeElement.value;
-    const amount = this.amountInputRef.nativeElement.value;
-    const ingredient = new Ingredient(name,amount);
+  onAddItem(form: NgForm) {
+
+    // do not need below lines because we using template driven now
+    /*const name = this.nameInputRef.nativeElement.value;
+    const amount = this.amountInputRef.nativeElement.value;*/
+
+    const data = form.value;
+    const ingredient = new Ingredient(data.name, data.amount);
     //this.ingredientAdded.emit(ingredient);
 
     this.shoppingLisService.addIngredients(ingredient);
